163140ac0305593366e364bd8abbbc47902214e5
Mark <mark@dev.haus>
2017-10-17 23:34:48 +0900

    Working on Themes

---
+++ ChainTicker.Shell/Helpers/ReflectionHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace ChainTicker.Shell.Helpers
+{
+    public class ReflectionHelpers
+    {
+        public static string GetEntryAssemblyVersion()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+
+            return entryAssembly?.GetName().Version.ToString();
+        }
+    }
+}
